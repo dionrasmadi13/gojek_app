1d32556b9a6848ce75a739ab4c5a4fc1563bdf04
dionrasmadi13 <dionrasmadi@gmail.com>
2020-08-19 14:38:12 +0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:gojek_app/constant.dart';
+import 'package:gojek_app/launcher/launcher_view.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Gojek',
+      theme: new ThemeData(
+        fontFamily: 'NeoSans',
+        primaryColor: GojekPalette.green,
+        accentColor: GojekPalette.green,
+      ),
+      home: new LauncherPage(),
+    );
+  }
+}
